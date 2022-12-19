00514ae032f3e098db84e0c9639a6a5b7d2b9aea
Gilben Ferolino <gilbenferolinoz@gmail.com>
2022-12-19 22:21:41 +0800

    Added dto for AddAttendance

---
+++ Samids-API/Samids-API/Dto/AddAttendanceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Samids_API.Dto
+{
+    public class AddAttendanceDto
+    {
+        public int studentId;
+        public DateTime date;
+        public DateTime actualTimeIn;
+        public DateTime actualTimeout;
+    }
+}
