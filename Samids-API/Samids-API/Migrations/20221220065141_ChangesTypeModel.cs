964231e9d7ee8cdd978ccc939b4ee51528c14d95
Gilben Ferolino <gilbenferolinoz@gmail.com>
2022-12-20 21:37:22 +0800

    New migrations

---
+++ Samids-API/Samids-API/Migrations/20221220065141_ChangesTypeModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SamidsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangesTypeModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Rfid",
+                table: "Students",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CurrentYear",
+                table: "Configs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Attendances",
+                type: "Date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Rfid",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CurrentYear",
+                table: "Configs",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Attendances",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "Date",
+                oldNullable: true);
+        }
+    }
+}
