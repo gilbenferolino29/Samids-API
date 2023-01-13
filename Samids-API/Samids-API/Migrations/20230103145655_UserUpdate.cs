4472cc58bb62d272e33604cdebd288a8e829b66b
Gilben Ferolino <gilbenferolinoz@gmail.com>
2023-01-13 13:24:18 +0800

    Migrations added for Password hashing

---
+++ Samids-API/Samids-API/Migrations/20230103145655_UserUpdate.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SamidsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "studentNo",
+                table: "Students",
+                newName: "StudentNo");
+
+            migrationBuilder.RenameColumn(
+                name: "facultyNo",
+                table: "Faculties",
+                newName: "FacultyNo");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "StudentNo",
+                table: "Students",
+                newName: "studentNo");
+
+            migrationBuilder.RenameColumn(
+                name: "FacultyNo",
+                table: "Faculties",
+                newName: "facultyNo");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
