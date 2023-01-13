1bbc0a1f6c660ebd8e51ba23bd67745b88af413a
Gilben Ferolino <gilbenferolinoz@gmail.com>
2023-01-13 13:22:07 +0800

    Added Auth, Config, Faculty - Controller

---
+++ Samids-API/Samids-API/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Samids_API.Dto;
+using Samids_API.Models;
+using Samids_API.Services.Interface;
+
+namespace Samids_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CRUDReturn>> Login (UserDto credentials)
+        {
+            return Ok(await _authService.Login(credentials));
+        }
+        [HttpPost]
+        public async Task<ActionResult<CRUDReturn>> Register(UserRegisterDto credentials)
+        {
+            return Ok(await _authService.Register(credentials));
+        }
+        [HttpPatch]
+        public async Task<ActionResult<CRUDReturn>> ChangePassword (string newPassword, User user)
+        {
+            return Ok(await _authService.ChangePassword(newPassword, user));
+        }
+    }
+}
---
+++ Samids-API/Samids-API/Controllers/ConfigController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Samids_API.Models;
+using Samids_API.Services.Interface;
+
+namespace Samids_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfigService _configService;
+
+        public ConfigController(IConfigService configService)
+        {
+            _configService = configService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CRUDReturn>> NewConfig(Config config)
+        {
+            return Ok(await _configService.NewConfig(config));
+        }
+
+
+    }
+}
+
